1d40726e3d4ef44337faf4f9dea54cd17016f538
selsarraj-create <selsarraj-create@users.noreply.github.com>
2026-02-04 22:49:12 +0000

    Fix: Use relative import for MetaPixel and ensure it renders in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MetaPixel />
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TKZLK55"
@@ -41,17 +42,8 @@ export default function RootLayout({
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-        <Script id="gtm" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TKZLK55');
-          `}
-        </Script>
-        <Toaster position="top-center" richColors />
         {children}
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   );
