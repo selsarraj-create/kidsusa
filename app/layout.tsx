5fff86d17cfdab08feeb26721e0f698508abb766
selsarraj-create <selsarraj-create@users.noreply.github.com>
2026-02-04 22:50:06 +0000

    Fix: Correctly update MetaPixel import to relative path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
-import { MetaPixel } from '@/components/MetaPixel';
+import { MetaPixel } from '../components/MetaPixel';
 import "./globals.css";
 
 const geistSans = Geist({
